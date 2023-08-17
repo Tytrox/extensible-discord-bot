fae154bab6891b5cac5de9da56abe7b30bdf6186
Jack Waller <jack@5barg8.com>
2023-08-17 13:35:14 +0100

    chore: only test in  directory

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     "^.+\\.ts?$": "ts-jest",
   },
   testEnvironment: "node",
-  testRegex: ".*/test/.*\\.(test|spec)?\\.(ts|tsx)$",
+  testRegex: "/test/.*\\.(test|spec)?\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   coverageThreshold: {
     global: {
